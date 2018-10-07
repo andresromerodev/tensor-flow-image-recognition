c554c5cdfdc136b9a50968312ed0139538eef852
Andrés Romero <andres.romero22@outlook.com>
2018-10-07 10:59:53 -0600

    Initial Files

---
+++ js/index.js
@@ -0,0 +1,56 @@
+$(document).ready( function() {
+
+    	$(document).on('change', '.btn-file :file', function() {
+		var input = $(this),
+			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [label]);
+		});
+
+		$('.btn-file :file').on('fileselect', function(event, label) {
+		    
+		    var input = $(this).parents('.input-group').find(':text'),
+		        log = label;
+		    
+		    if( input.length ) {
+		        input.val(log);
+		    } else {
+		        if( log ) alert(log);
+		    }
+	    
+		});
+
+		function readURL(input) {
+		    if (input.files && input.files[0]) {
+		        var reader = new FileReader();
+		        
+		        reader.onload = function (e) {
+					$('#img-upload').attr('src', e.target.result);
+		        }
+		        
+		        reader.readAsDataURL(input.files[0]);
+			}
+		}
+
+		$("#imgInp").change(function(){
+			readURL(this);
+		}); 	
+
+		$("#indentify-btn").click(function (e) { 
+			e.preventDefault();
+			var text = "";
+			const img = document.getElementById('img-upload');
+			// Load the model:
+			mobilenet.load().then(model => {
+				// Classify the image:
+				model.classify(img).then(predictions => {
+
+					predictions.forEach(prediction => {
+						text = text + "Identified Object: " + prediction.className + "\n" + "Probability: "  + Math.round(prediction.probability * 100) + "% \n\n";
+					});
+
+					$("#results-field").text(text);
+				});
+			});
+		});
+});
+
